d19c0783eb9750c701158fe8926034beb104c28c
Maksym Shmelov <maksym.shamelyov@gmail.com>
2022-06-27 13:43:57 +0300

    added build

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
         mode: "development",
         output: {
             filename: '[name].[hash].js',
-            path: path.resolve(__dirname, 'dist'),
+            path: path.resolve(__dirname, 'docs'),
         },
         devtool: 'inline-source-map',
         devServer: {
